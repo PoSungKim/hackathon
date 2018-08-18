6e385ead57a2ae031da5a3a4527693f2151ab0bc
YOUR NAME <YOUR@EMAIL>
2018-08-19 04:30:35 +0900

    details

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,16 @@ Rails.application.routes.draw do
   end
   #root 'restaurants#index'
 
+  ## 로그인과 관리자 페이지
   devise_for :users, :controllers => { omniauth_callbacks: 'user/omniauth_callbacks', registraions: 'user/registrations'}
   devise_scope :user do
     get 'editsns', :to => 'devise/registrations#editsns'
   end
-  ######### 임시
   get 'home/index'
+  ######### 임시
   root 'home#index' 
   ##########
-
+  devise_for :admins
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180817032317) do
+ActiveRecord::Schema.define(version: 20180818185349) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "allergies", force: :cascade do |t|
     t.string   "menu_name"
@@ -87,6 +99,7 @@ ActiveRecord::Schema.define(version: 20180817032317) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean  "admin",                  default: false
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "name",                   default: "",    null: false
