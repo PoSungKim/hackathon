c8909233c08c0cd7a06dbb9091447ed3738cef06
ssng123 <heeya121hs@likelion.org>
2018-08-21 16:42:24 +0900

    index page

--- db/schema.rb
+++ db/schema.rb
@@ -12,48 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20180819075248) do
 
-  create_table "allergies", force: :cascade do |t|
-    t.string   "menu_name"
-    t.integer  "a1_maemil"
-    t.integer  "a2_mil"
-    t.integer  "a3_daedu"
-    t.integer  "a4_hodu"
-    t.integer  "a5_ddangkong"
-    t.integer  "a6_peach"
-    t.integer  "a7_tomato"
-    t.integer  "a8_piggogi"
-    t.integer  "a9_nanryu"
-    t.integer  "a10_milk"
-    t.integer  "a11_ddakgogi"
-    t.integer  "a12_shoigogi"
-    t.integer  "a13_saewoo"
-    t.integer  "a14_godeungeoh"
-    t.integer  "a15_honghap"
-    t.integer  "a16_junbok"
-    t.integer  "a17_gul"
-    t.integer  "a18_jogaeryu"
-    t.integer  "a19_gye"
-    t.integer  "a20_ohjingeoh"
-    t.integer  "a21_ahwangsan"
-    t.string   "restaurant_name"
-    t.integer  "restaurant_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "image"
-    t.index ["restaurant_id"], name: "index_allergies_on_restaurant_id"
-  end
-
-  create_table "menumatches", force: :cascade do |t|
-    t.integer  "restaurant_id"
-    t.string   "restaurant_name"
-    t.integer  "menu_id"
-    t.string   "menu_name"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["menu_id"], name: "index_menumatches_on_menu_id"
-    t.index ["restaurant_id"], name: "index_menumatches_on_restaurant_id"
-  end
-
   create_table "menus", force: :cascade do |t|
     t.string   "menu_name"
     t.integer  "a1_maemil"
@@ -77,20 +35,16 @@ ActiveRecord::Schema.define(version: 20180819075248) do
     t.integer  "a19_gye"
     t.integer  "a20_ohjingeoh"
     t.integer  "a21_ahwangsan"
+    t.integer  "restaurant_id"
     t.string   "restaurant_name"
     t.string   "image"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "restaurant_name"
-    t.string   "sido"
-    t.string   "sigungu"
-    t.string   "doromyeong"
-    t.integer  "gunmul_bunho1"
-    t.integer  "gunmul_bunho2"
-    t.string   "phone_number"
     t.integer  "a1_maemil"
     t.integer  "a2_mil"
     t.integer  "a3_daedu"
