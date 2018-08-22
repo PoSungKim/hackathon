acf5575199c348e146a3020b9705a9273e36d803
PoSungKim <psungkr94@likelion.org>
2018-08-22 15:05:10 +0900

    알레르기 검색에 라이크 기능 추가

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,7 +8,7 @@ class FollowsController < ApplicationController
         else !@article.followed_by?(current_user)
             current_user.follow(@article)
         end 
-            redirect_to article_path(@article.id)
+            # redirect_to article_path(@article.id)
     end 
 
     def zizum_front_follow_toggle
@@ -20,7 +20,7 @@ class FollowsController < ApplicationController
         else !@zizum.followed_by?(current_user)
             current_user.follow(@zizum)
         end 
-            redirect_to menus_path(id: @zizum.id, utf8: params[:utf8], sido: params[:sido] , sigungu: params[:sigungu], a9_nanryu: params[:a9_nanryu], a6_peach: params[:a6_peach], a7_tomato: params[:a7_tomato], a1_maemil: params[:a1_maemil], a2_mil: params[:a2_mil], a3_daedu: params[:a3_daedu], a11_ddakgogi: params[:a11_ddakgogi], a12_shoigogi: params[:a12_shoigogi], a8_piggogi: params[:a8_piggogi], a13_saewoo: params[:a13_saewoo], a14_godeungeoh: params[:a14_godeungeoh], a15_honghap: params[:a15_honghap], a16_junbok: params[:a16_junbok], a17_gul: params[:a17_gul], a18_jogaeryu: params[:a18_jogaeryu], a19_gye: params[:a19_gye], a20_ohjingeoh: params[:a20_ohjingeoh], a4_hodu: params[:a4_hodu], a5_ddangkong: params[:a5_ddangkong], a21_ahwangsan: params[:a21_ahwangsan], commit: params[:commit])
+            # redirect_to zizum_path(@zizum.id)
     end
 
     def zizum_back_follow_toggle
@@ -32,6 +32,6 @@ class FollowsController < ApplicationController
         else !@zizum.followed_by?(current_user)
             current_user.follow(@zizum)
         end 
-            redirect_to zizuminfo_path(@zizum.id) 
+            redirect_to profile_path(current_user.id) 
     end
 end 
